ef5a3f92a572e3e748c3cf6df3d62e90413decb8
Julia Lang <julia.al.lang@gmail.com>
2021-04-28 14:07:32 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Julia Lang on 4/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  testerApp.swift
+//  Shared
+//
+//  Created by Julia Lang on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
